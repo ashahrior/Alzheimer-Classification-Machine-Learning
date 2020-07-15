6dfab25ec3f78708fce427cceaeeff78edb27263
Ashef Shahrior <ashahrior.cse.mbstu@gmail.com>
2020-07-16 01:29:42 +0600

    CLAHE added

--- glcm_management.py
+++ glcm_management.py
@@ -16,6 +16,10 @@ imputed_data_fold = r"E:\THESIS\ADNI_data\ADNI1_Annual_2_Yr_3T_306_WORK\INTEREST
 
 intrst_data_fol = r"E:\THESIS\ADNI_data\ADNI1_Annual_2_Yr_3T_306_WORK\INTEREST_NPY_DATA\\"
 
+clahe_folder = r"E:\THESIS\ADNI_data\ADNI1_Annual_2_Yr_3T_306_WORK\INTEREST_NPY_DATA\CLAHE_NPY\CLAHE_{}npy\\"
+
+clahe_glcm = r"E:\THESIS\ADNI_data\ADNI1_Annual_2_Yr_3T_306_WORK\INTEREST_NPY_DATA\CLAHE_NPY\CLAHE_GLCM\CLAHE_glcm_{}\\"
+
 
 limit = 0
 
@@ -44,24 +48,27 @@ def save_glcm_feats(case, serial, features):
     en = features[3]
     corr = features[4]
     asms = features[5]
+    
+    #save_as = glcm_feats
+    save_as = clahe_glcm
 
-    np.save((glcm_feats.format(case) + "{}-asm{}".format(case, serial)), asms)
-    print(f'{case}-{serial} asm', 'saved')
+    np.save((save_as.format(case) + "{}-clahe-asm{}".format(case, serial)), asms)
+    print(f'{case}-{serial}-clahe asm', 'saved')
 
-    np.save((glcm_feats.format(case) + "{}-contrast{}".format(case, serial)), con)
-    print(f'{case}-{serial} contrast', 'saved')
+    np.save((save_as.format(case) + "{}-clahe-contrast{}".format(case, serial)), con)
+    print(f'{case}-{serial}-clahe contrast', 'saved')
 
-    np.save((glcm_feats.format(case) + "{}-correlation{}".format(case, serial)), corr)
-    print(f'{case}-{serial} correlation', 'saved')
+    np.save((save_as.format(case) + "{}-clahe-correlation{}".format(case, serial)), corr)
+    print(f'{case}-{serial}-clahe correlation', 'saved')
 
-    np.save((glcm_feats.format(case) + "{}-dissimlarity{}".format(case, serial)), diss)
-    print(f'{case}-{serial} dissimilarity', 'saved')
+    np.save((save_as.format(case) + "{}-clahe-dissimlarity{}".format(case, serial)), diss)
+    print(f'{case}-{serial}-clahe dissimilarity', 'saved')
 
-    np.save((glcm_feats.format(case) + "{}-energy{}".format(case, serial)), en)
-    print(f'{case}-{serial} energy', 'saved')
+    np.save((save_as.format(case) + "{}-clahe-energy{}".format(case, serial)), en)
+    print(f'{case}-{serial}-clahe energy', 'saved')
 
-    np.save((glcm_feats.format(case) + "{}-homogeneity{}".format(case, serial)), homo)
-    print(f'{case}-{serial} homogeneity', 'saved')
+    np.save((save_as.format(case) + "{}-clahe-homogeneity{}".format(case, serial)), homo)
+    print(f'{case}-{serial}-clahe homogeneity', 'saved')
     return
 
 
@@ -98,8 +105,7 @@ def get_glcm(case, serial, data):
     corr = []
     asms = []
     for i in range(data.shape[0]):
-        matrix_coocurrence = greycomatrix(data[i], [1], [
-                                          0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=256, normed=False, symmetric=False)
+        matrix_coocurrence = greycomatrix(data[i], [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=256, normed=False, symmetric=False)
 
         asm = get_asm_feature(matrix_coocurrence)
         asms.append(asm.flatten())
@@ -126,12 +132,16 @@ def get_glcm(case, serial, data):
 
 
 def calc_glcm(case):
-    os.chdir(norm_data.format(case))
-    for file in os.listdir():
-        data = np.load(file, allow_pickle=True)
-        print(file, ' -> ', data.shape)
-        serial = re.findall('\d+', file)[0]
-        get_glcm(case, serial, data)
+    #os.chdir(norm_data.format(case))
+    for case in cases:
+        os.chdir(clahe_folder.format(case))
+        for file in os.listdir():
+            fname, fext = os.path.splitext(file)
+            data = np.load(file, allow_pickle=True)
+            print(file, ' -> ', data.shape)
+            serial = re.findall('\d+', file)[0]
+            get_glcm(case, serial, data)
+    return
 
 
 def get_max_slice(src):
@@ -148,6 +158,7 @@ def get_max_slice(src):
             data = np.load(file)
             slices = data.shape[0]
             limit = max(slices, limit)
+        print(f'{c} MAX- {limit}')
     return limit
 
 
@@ -185,7 +196,8 @@ def interpolate_data(data):
 def perform_data_generation(cases=['AD', 'CN', 'MCI']):
     global limit
     for c in cases:
-        os.chdir(glcm_feats.format(c))
+        #os.chdir(glcm_feats.format(c))
+        os.chdir(clahe_glcm.format(c))
         count = 0
         for file in os.listdir():
             print(file, '->', end='')
@@ -199,8 +211,8 @@ def perform_data_generation(cases=['AD', 'CN', 'MCI']):
             print(f'{fname} interpolation done.')
 
             interpolated_data = interpolated_df.to_numpy()
-            np.save(imputed_data_fold.format(c) +
-                    f"{fname}-imp{fext}", interpolated_data)
+            #np.save(imputed_data_fold.format(c) + f"{fname}-imp{fext}", interpolated_data)
+            np.save(clahe_glcm.format(c) + f"{fname}-imp{fext}", interpolated_data)
 
             print(f"{fname}-imp{fext} saved.")
             count += 1
@@ -227,6 +239,7 @@ def check_data(cases=['AD', 'CN', 'MCI']):
 def merge_glcm():
     """merge all the glcm feats of all three cases into a single file in 2D
     """
+    case_data = []
     for case in ['AD', 'CN', 'MCI']:
         if case == 'AD':
             target = 1
@@ -239,13 +252,13 @@ def merge_glcm():
             print(f'{case} #{serial} file - ', end='')
             for feat in feats:
                 print(f'{feat} -', end=' ')
-                form = f"{case}-{feat}{serial}-imp.npy"
-                data = np.load(imputed_data_fold.format(case) + form, allow_pickle=True)
+                form = f"{case}-clahe-{feat}{serial}-imp.npy"
+                #data = np.load(imputed_data_fold.format(case) + form, allow_pickle=True)
+                data = np.load(clahe_glcm.format(case) + form ,  allow_pickle=True)
                 data = data.flatten()
                 d.append(data)
             file_serial_data = np.concatenate(d)
             file_serial_data = np.append(file_serial_data, [target])
-
             #print(file_serial_data)
             #print(file_serial_data.shape)
             case_data.append(file_serial_data)
@@ -254,9 +267,16 @@ def merge_glcm():
 
     case_data_array = np.array(case_data)
     print(case_data_array.shape)
-    np.save(intrst_data_fol+"all_clean_glcm_54.npy", case_data_array)
+    np.save(intrst_data_fol+"clahe_glcm_54.npy", case_data_array)
 
 
 
 if __name__ == "__main__":
+    #calc_glcm(['AD','CN','MCI'])
+    limit = 0
+    #limit = get_max_slice(r"E:\THESIS\ADNI_data\ADNI1_Annual_2_Yr_3T_306_WORK\INTEREST_NPY_DATA\CLAHE_NPY\CLAHE_GLCM\CLAHE_glcm_{}\\")
+    limit = 69
+    #perform_data_generation()
+    #check_data()
+    #merge_glcm()
     pass
